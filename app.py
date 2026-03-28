bbe03a4e50ae4c35ee24d5ff450b29ef28acc046
Edixon19 <edixondiaz16@gmail.com>
2026-03-28 11:07:50 -0500

    primera fase

---
+++ app.py
@@ -0,0 +1,10 @@
+# .gitignore
+# git config  --global user.email "edixondiaz16@gmail.com"
+# git config --global user.name "Edixon19"
+# git remote add origin https://github.com/Edixon19/OFICIAL_PROYECTO-.git
+
+import streamlit as st
+st.title ("Mi Proyecto")
+
+
+
